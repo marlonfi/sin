3315131c34947c5a02c0f2d0501256b3bf0d44a4
WENCESLAO PAEZ CHAVEZ <wcpaez@gmail.com>
2014-01-17 12:03:12 -0500

    Add dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Sinesss::Application.routes.draw do
+  get "/dashboard", to: 'home#dashboard'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
