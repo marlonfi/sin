004bde686f675e2fc86c650187d7215ef102a468
WENCESLAO PAEZ CHAVEZ <wcpaez@gmail.com>
2014-01-17 16:59:47 -0500

    Add font awesome

--- config/application.rb
+++ config/application.rb
@@ -29,7 +29,9 @@ module Sinesss
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
-    config.assets.precompile = ['*.js', '*.css']
+    config.assets.precompile = ['*.js', '*.css', '*.jpg','*.jpeg', '*.png',
+                                '*.gif','*.otf', '*.eot','*.svg', '*.ttf',
+                                '*.woff','*.ico']
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
