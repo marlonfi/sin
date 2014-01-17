bdf7cd6889611f030154ea6e28d7dab07d7235a0
WENCESLAO PAEZ CHAVEZ <wcpaez@gmail.com>
2014-01-17 11:51:50 -0500

    Inicio proyecto sinesss-core

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sinesss::Application.config.secret_key_base = '7c92a4f7141e154e2e1adbcd64bb45b3b180c1a45f0363e26fd54623135a8e995f13c38cf7e44f3c8502dd1aab796a25540c6fce4a41c556cb2807bbd5f94409'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sinesss::Application.config.session_store :cookie_store, key: '_sinesss_session'
