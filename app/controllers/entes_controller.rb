ef55fab44382ac8a09b5ad382f5bd1d3b0fc583e
WENCESLAO PAEZ CHAVEZ <wcpaez@gmail.com>
2014-01-29 16:23:37 -0500

    Add validation to view if a red is created for create a ente

--- app/controllers/entes_controller.rb
+++ app/controllers/entes_controller.rb
@@ -35,7 +35,7 @@ class EntesController < ApplicationController
     importacion = Import.new(archivo: params[:archivo], tipo_clase: 'Entes',
                             descripcion: params[:descripcion], formato_org: 'ESSALUD')
     if importacion.save
-      Ente.import(importacion)
+      Ente.delay.import(importacion)
       redirect_to dashboard_path, notice:'El proceso de importacion durará unos minutos.'
     else
       redirect_to entes_path, alert: 'El archivo es muy grande, o tiene un formato incorrecto.'
--- app/models/ente.rb
+++ app/models/ente.rb
@@ -15,58 +15,76 @@ class Ente < ActiveRecord::Base
 		  if row['PROGRAMA'] == 'SALUD'
 		  	if row['SUB PROGRA'][0..2] == 'RA '
 		  		red = RedAsistencial.find_by_cod_essalud(row['SUB PROGRA'])
-		  		case row['ACTIVIDAD']
-		  		when 'Ger.Red Asist.'
-		  			ente = find_or_create_by(cod_essalud: "Ger.Red Asist."+"-"+ red.cod_essalud)
-		  			red.entes << ente
-		  		when 'Dir.Red Asist'
-		  			ente = find_or_create_by(cod_essalud: "Dir.Red Asist"+"-"+ red.cod_essalud)
-		  			red.entes << ente
-		  		when 'Dir.Red Asist.'
-		  			ente = find_or_create_by(cod_essalud: "Dir.Red Asist"+"-"+ red.cod_essalud)
-		  			red.entes << ente 
-		  		else
-		  			if row['ACTIVIDAD'] == 'PM San Miguel'
-		  				ente = find_or_create_by(cod_essalud: "PM San Miguel"+"-"+ red.cod_essalud)
-		  				red.entes << ente 		  				
-		  			else
-		  				ente = find_or_create_by(cod_essalud: row['ACTIVIDAD'])
-		  				red.entes << ente
-		  			end
-		  		end	
+		  		if red
+			  		case row['ACTIVIDAD']
+			  		when 'Ger.Red Asist.'
+			  			ente = find_or_create_by(cod_essalud: "Ger.Red Asist."+"-"+ red.cod_essalud)
+			  			red.entes << ente
+			  		when 'Dir.Red Asist'
+			  			ente = find_or_create_by(cod_essalud: "Dir.Red Asist"+"-"+ red.cod_essalud)
+			  			red.entes << ente
+			  		when 'Dir.Red Asist.'
+			  			ente = find_or_create_by(cod_essalud: "Dir.Red Asist"+"-"+ red.cod_essalud)
+			  			red.entes << ente 
+			  		else
+			  			if row['ACTIVIDAD'] == 'PM San Miguel'
+			  				ente = find_or_create_by(cod_essalud: "PM San Miguel"+"-"+ red.cod_essalud)
+			  				red.entes << ente 		  				
+			  			else
+			  				ente = find_or_create_by(cod_essalud: row['ACTIVIDAD'])
+			  				red.entes << ente
+			  			end
+			  		end
+			  	end	
 		  	elsif row['SUB PROGRA'] == 'INCOR'
 		  		red = RedAsistencial.find_by_cod_essalud('ORG. DESCONCENTRADOS')
-		  		ente = find_or_create_by(cod_essalud: 'INCOR')
-		  		red.entes << ente
+		  		if red
+			  		ente = find_or_create_by(cod_essalud: 'INCOR')
+			  		red.entes << ente
+			  	end
 		  	elsif row['SUB PROGRA'] == 'CN Salud Rena'
 		  		red = RedAsistencial.find_by_cod_essalud('ORG. DESCONCENTRADOS')
-		  		ente = find_or_create_by(cod_essalud: 'CN Salud Rena')
-		  		red.entes << ente
+		  		if red
+			  		ente = find_or_create_by(cod_essalud: 'CN Salud Rena')
+			  		red.entes << ente
+			  	end
 		  	elsif row['SUB PROGRA'] == 'Programa CEI'
 		  		red = RedAsistencial.find_by_cod_essalud('ORG. DESCONCENTRADOS')
-		  		ente = find_or_create_by(cod_essalud: 'Programa CEI')
-		  		red.entes << ente
+		  		if red
+			  		ente = find_or_create_by(cod_essalud: 'Programa CEI')
+			  		red.entes << ente
+			  	end
 		  	elsif row['SUB PROGRA'] == 'Def.del Asegu'
-					red = RedAsistencial.find_by_cod_essalud(row['SUB ACTIV1'])
-					ente =  find_or_create_by(cod_essalud: 'Def.del Asegu' +'-'+ red.cod_essalud)
-					red.entes << ente
+		  		red = RedAsistencial.find_by_cod_essalud(row['SUB ACTIV1'])
+					if red
+						ente =  find_or_create_by(cod_essalud: 'Def.del Asegu' +'-'+ red.cod_essalud)
+						red.entes << ente
+					end
 				elsif row['SUB PROGRA'] == 'GC Pre.Soc.y'
 					red = RedAsistencial.find_by_cod_essalud(row['SUB ACTIV1'])
-					ente =  find_or_create_by(cod_essalud: 'GC Pre.Soc.y' +'-'+ red.cod_essalud)
-					red.entes << ente
+					if red
+						ente =  find_or_create_by(cod_essalud: 'GC Pre.Soc.y' +'-'+ red.cod_essalud)
+						red.entes << ente
+					end
 				elsif row['SUB PROGRA'] == 'Org.Ctrol.Ins'
 					red = RedAsistencial.find_by_cod_essalud(row['ACTIVIDAD'])
-					ente =  find_or_create_by(cod_essalud: 'Org.Ctrol.Ins' +'-'+ red.cod_essalud)
-					red.entes << ente		  		
+					if red
+						ente =  find_or_create_by(cod_essalud: 'Org.Ctrol.Ins' +'-'+ red.cod_essalud)
+						red.entes << ente	
+					end	  		
 		  	elsif row['SUB PROGRA'] == 'GC Prest Salu'
 		  		red = RedAsistencial.find_by_cod_essalud('ORG. DESCONCENTRADOS')
 		  		if row['SUB ACTIV1'] == 'Despacho'
-		  			ente = find_or_create_by(cod_essalud: 'Despacho-GCPS')
-		  			red.entes << ente
-		  		else	
-		  			ente = find_or_create_by(cod_essalud: row['SUB ACTIV1'])
-		  			red.entes << ente
-		  		end
+		  			if red
+			  			ente = find_or_create_by(cod_essalud: 'Despacho-GCPS')
+			  			red.entes << ente
+			  		end
+		  		else
+			  		if red	
+			  			ente = find_or_create_by(cod_essalud: row['SUB ACTIV1'])
+			  			red.entes << ente
+			  		end
+			  	end
 		  	end	
 		  else
 		  	red = RedAsistencial.find_by_cod_essalud('ORG. DESCONCENTRADOS')
